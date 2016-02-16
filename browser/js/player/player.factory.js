f3ab9cc49b031d4a1971981fd43e58533e52fc41
zhengshizhao <zhao.zhengshi@gmail.com>
2016-02-16 17:08:22 -0500

    add timeformat

--- browser/js/player/player.factory.js
+++ browser/js/player/player.factory.js
@@ -7,6 +7,7 @@ juke.factory('PlayerFactory', function ($rootScope) {
   var playing = false,
       currentSong = null,
       currentList = [],
+      playtime = 0,
       progress = 0;
 
   // initialize the audio element
@@ -64,6 +65,10 @@ juke.factory('PlayerFactory', function ($rootScope) {
     return progress;
   };
 
+  player.getTime = function(){
+    return playtime;
+  }
+
   // audio event listening
 
   audio.addEventListener('ended', function () {
@@ -73,6 +78,19 @@ juke.factory('PlayerFactory', function ($rootScope) {
 
   audio.addEventListener('timeupdate', function () {
     progress = audio.currentTime / audio.duration;
+    playtime = timeFormat(audio.currentTime) + "   /  " + timeFormat(audio.duration);
+    
+    function timeFormat(timeInSecond){ 
+      var timeInMins = "",
+          mm = 0,
+          ss = 0;
+          mm = Math.floor(timeInSecond / 60) < 10 ? "0"+Math.floor(timeInSecond / 60) : Math.floor(timeInSecond / 60) ;
+
+          ss = (timeInSecond % 60).toFixed(0) < 10 ? "0"+(timeInSecond % 60).toFixed(0) :(timeInSecond % 60).toFixed(0);
+          timeInMins = mm + ":" + ss;
+      return timeInMins
+    }
+
     $rootScope.$evalAsync();
   });
 
--- browser/js/sidebar/sidebar.controller.js
+++ browser/js/sidebar/sidebar.controller.js
@@ -1,7 +1,9 @@
 'use strict';
 
-juke.controller('SidebarCtrl', function ($scope) {
+juke.controller('SidebarCtrl', function ($scope, PlayerFactory) {
 
+	$scope.getCurrentSong = PlayerFactory.getCurrentSong;
+	$scope.getTime = PlayerFactory.getTime;
   // nothing to see here for now… state transitions happening with ui-sref!
 
 });
