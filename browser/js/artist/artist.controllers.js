c6363e25650717cb56c82809a3145e8b872f733e
Terry Horowitz <terryhorowitz@Terrys-MacBook-Air.local>
2016-02-16 13:02:02 -0500

    trimmed down controllers

--- browser/js/artist/artist.controllers.js
+++ browser/js/artist/artist.controllers.js
@@ -14,22 +14,5 @@ juke.controller('ArtistCtrl', function ($scope, $log, PlayerFactory, theArtist) 
 
   $scope.artist = theArtist;
 
-  $scope.getCurrentSong = function () {
-    return PlayerFactory.getCurrentSong();
-  };
-
-  $scope.isPlaying = function (song) {
-    return PlayerFactory.isPlaying() && PlayerFactory.getCurrentSong() === song;
-  };
-
-  $scope.toggle = function (song) {
-    if (song !== PlayerFactory.getCurrentSong()) {
-      PlayerFactory.start(song, $scope.artist.songs);
-    } else if ( PlayerFactory.isPlaying() ) {
-      PlayerFactory.pause();
-    } else {
-      PlayerFactory.resume();
-    }
-  };
 
 });
--- browser/js/song-list.directive.js
+++ browser/js/song-list.directive.js
@@ -1,11 +1,30 @@
 'use strict';
 
-juke.directive('songList', function (){
+juke.directive('songList', function (PlayerFactory){
   return {
     restrict: 'E',
     templateUrl: 'js/song-list.html',
     scope: {
       type: '='
+    },
+    link: function(scope) {
+        scope.getCurrentSong = function () {
+        return PlayerFactory.getCurrentSong();
+      };
+
+      scope.isPlaying = function (song) {
+        return PlayerFactory.isPlaying() && PlayerFactory.getCurrentSong() === song;
+      };
+
+      scope.toggle = function (song) {
+        if (song !== PlayerFactory.getCurrentSong()) {
+          PlayerFactory.start(song, scope.type.songs);
+        } else if ( PlayerFactory.isPlaying() ) {
+          PlayerFactory.pause();
+        } else {
+          PlayerFactory.resume();
+        }
+      };
     }
   }
 })
